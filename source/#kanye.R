7662276130afab4fc5c2997ea876f446e7afd45a
Riley <reachroach25@icloud.com>
2018-06-27 20:36:30 -0500

    wordclouds
    lookin' at some tweets

--- source/#kanye.R
+++ source/#kanye.R
@@ -9,3 +9,43 @@ kanye <-
   n = 2000,
   include_rts = FALSE
 )
+
+kanye_tbl <-
+  kanye %>% 
+  unnest_tokens(word,
+                text)
+
+kanye_tbl <-
+  kanye_tbl %>% 
+  anti_join(stop_words)
+
+kanye_tbl <-
+  kanye_tbl %>% 
+  count(word,
+        sort = TRUE)
+
+kanye_tbl <-
+  kanye_tbl %>% 
+  filter(
+    !word %in% c(
+      't.co',
+      'https',
+      'tvtime',
+      'watched',
+      'watching',
+      'watch',
+      'la',
+      "it's",
+      'el',
+      'en',
+      'tv',
+      'je',
+      'ep',
+      'week',
+      'amp',
+      'de'
+    )
+  )
+
+wordcloud2::wordcloud2(kanye_tbl,
+                       size = 1.5)
